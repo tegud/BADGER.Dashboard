4fb6b61d7cd3b40cda8906762d1ec6d850ef7b69
Steve Elliott <steve@tegud.net>
2013-09-10 22:51:11 +0100

    Support for multiple component types

--- Badger.Dashboard/Assets/js/status/TLRGRP.BADGER.Dashboard.Builder.js
+++ Badger.Dashboard/Assets/js/status/TLRGRP.BADGER.Dashboard.Builder.js
@@ -1,22 +1,33 @@
 TLRGRP.namespace('TLRGRP.BADGER.Dashboard');
 
 (function () {
-	TLRGRP.BADGER.Dashboard.Builder = function(element, config) {
-		var buildGraph = function(item) {
-				return $('<div />', {
-						class: 'graph-container' + (item.class ? ' ' + item.class : '')
-					})
-					.append($('<div />', {
-						class: 'graph-title',
-						text: item.title
-					}))
-					.append($('<iframe />', {
-						src: 'assets/graph.html?expressions=' + encodeURIComponent(JSON.stringify(item.expressions)) 
-							+ '&chartOptions=' + encodeURIComponent(JSON.stringify(item.chartOptions)) ,
-						frameborder: '0'
-					}));
-			};
-			
-		element.append($.map(config, buildGraph));
+    var components = {
+        'chart': function (item) {
+            return $('<div />', {
+                'class': 'graph-container' + (item.class ? ' ' + item.class : '')
+            })
+                .append($('<div />', {
+                    'class': 'graph-title',
+                    text: item.title
+                }))
+                .append($('<iframe />', {
+                    src: 'assets/graph.html?expressions=' + encodeURIComponent(JSON.stringify(item.expressions))
+                        + '&chartOptions=' + encodeURIComponent(JSON.stringify(item.chartOptions)),
+                    frameborder: '0'
+                }));
+        }
+    };
+
+    TLRGRP.BADGER.Dashboard.Builder = function (element, config) {
+        _(config).each(function (item) {
+            var component = item.component || 'chart';
+            var componentBuilder = components[component];
+
+            if (!componentBuilder) {
+                return true;
+            }
+
+            element.append(componentBuilder(item));
+        });
 	};
 })();
