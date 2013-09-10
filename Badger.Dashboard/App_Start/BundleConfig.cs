0cfbf874a2cf196e62bda7499047f81e363acf75
Steve Elliott <steve@tegud.net>
2013-09-10 23:14:18 +0100

    removing .min extensions temporarily
    ASP.NET Web Optimization ignores .min files in debug, need to look into
    more.

--- Badger.Dashboard/App_Start/BundleConfig.cs
+++ Badger.Dashboard/App_Start/BundleConfig.cs
@@ -6,57 +6,59 @@ namespace BADGER.Dashboard.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/Assets/js/bundles/core")
+                .Include("~/Assets/js/ladda.js")
+                .Include("~/Assets/js/menu.js")
+                .Include("~/Assets/js/lib/moment.js")
+                .Include("~/Assets/js/lib/jquery-2.0.0.js")
+                .Include("~/Assets/js/lib/lodash.js")
+                .Include("~/Assets/js/lib/TLRGRP.core.js")
+                .Include("~/Assets/js/lib/nanomachine.js"));
             var dashboardsBundle = BuildDashboardsBundle();
-
             bundles.Add(dashboardsBundle);
         }
 
         private static ScriptBundle BuildDashboardsBundle()
         {
-            var dashboardsBundle = new ScriptBundle("~/assets/js/bundles/dashboards");
+            var dashboardsBundle = new ScriptBundle("~/Assets/js/bundles/dashboards");
 
-            dashboardsBundle.Include("~/assets/js/lib/jquery-2.0.0.min.js",
-                                     "~/assets/js/lib/jquery-ui.js",
-                                     "~/assets/js/lib/lodash.js",
-                                     "~/assets/js/lib/TLRGRP.core.js",
-                                     "~/assets/js/lib/moment.min.js",
-                                     "~/assets/js/lib/nanomachine.js",
-                                     "~/assets/js/lib/TLRGRP.BADGER.utilities.js");
+            dashboardsBundle.Include("~/Assets/js/lib/jquery-ui.js",
+                                     "~/Assets/js/lib/TLRGRP.BADGER.utilities.js");
 
-            dashboardsBundle.Include("~/assets/js/status-charts/TLRGRP.dashboards.Builder.js");
+            dashboardsBundle.Include("~/Assets/js/status-charts/TLRGRP.dashboards.Builder.js");
 
-            dashboardsBundle.Include("~/assets/js/status/UI/TLRGRP.BADGER.ColorPalette.js",
-                                     "~/assets/js/status/UI/TLRGRP.BADGER.DashboardList.js",
-                                     "~/assets/js/status/UI/TLRGRP.BADGER.TimeSelect.js");
+            dashboardsBundle.Include("~/Assets/js/status/UI/TLRGRP.BADGER.ColorPalette.js",
+                                     "~/Assets/js/status/UI/TLRGRP.BADGER.DashboardList.js",
+                                     "~/Assets/js/status/UI/TLRGRP.BADGER.TimeSelect.js");
 
-            dashboardsBundle.Include("~/assets/js/status/Sources/TLRGRP.BADGER.Cube.js");
+            dashboardsBundle.Include("~/Assets/js/status/Sources/TLRGRP.BADGER.Cube.js");
 
-            dashboardsBundle.Include("~/assets/js/status/TLRGRP.BADGER.Machines.js",
-                                     "~/assets/js/status/TLRGRP.BADGER.Pages.js",
-                                     "~/assets/js/status/LateRooms/TLRGRP.BADGER.LateRooms.Machines.js",
-                                     "~/assets/js/status/LateRooms/TLRGRP.BADGER.LateRooms.Pages.js");
+            dashboardsBundle.Include("~/Assets/js/status/TLRGRP.BADGER.Machines.js",
+                                     "~/Assets/js/status/TLRGRP.BADGER.Pages.js",
+                                     "~/Assets/js/status/LateRooms/TLRGRP.BADGER.LateRooms.Machines.js",
+                                     "~/Assets/js/status/LateRooms/TLRGRP.BADGER.LateRooms.Pages.js");
 
-            dashboardsBundle.Include("~/assets/js/status/Metrics/TLRGRP.BADGER.IIS.js",
-                                     "~/assets/js/status/Metrics/TLRGRP.BADGER.WMI.js",
-                                     "~/assets/js/status/Metrics/TLRGRP.BADGER.Errors.js");
+            dashboardsBundle.Include("~/Assets/js/status/Metrics/TLRGRP.BADGER.IIS.js",
+                                     "~/Assets/js/status/Metrics/TLRGRP.BADGER.WMI.js",
+                                     "~/Assets/js/status/Metrics/TLRGRP.BADGER.Errors.js");
 
-            dashboardsBundle.Include("~/assets/js/status/TLRGRP.BADGER.Dashboard.GraphFactory.js",
-                                     "~/assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.js",
-                                     "~/assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.IIS.js",
-                                     "~/assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.Errors.js");
+            dashboardsBundle.Include("~/Assets/js/status/TLRGRP.BADGER.Dashboard.GraphFactory.js",
+                                     "~/Assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.js",
+                                     "~/Assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.IIS.js",
+                                     "~/Assets/js/status/Graphs/TLRGRP.BADGER.Dashboard.Graphs.Errors.js");
 
-            dashboardsBundle.Include("~/assets/js/status/TLRGRP.BADGER.DashboardCollection.js",
-                "~/assets/js/status/TLRGRP.BADGER.Dashboard.Builder.js");
+            dashboardsBundle.Include("~/Assets/js/status/TLRGRP.BADGER.DashboardCollection.js",
+                "~/Assets/js/status/TLRGRP.BADGER.Dashboard.Builder.js");
 
-            dashboardsBundle.Include("~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.GraphSet.js",
-                                     "~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.WebHosts.js",
-                                     "~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.ByHost.js",
-                                     "~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.Overview.js",
-                                     "~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.Mobile.js",
-                                     "~/assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.ByPage.js");
+            dashboardsBundle.Include("~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.GraphSet.js",
+                                     "~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.WebHosts.js",
+                                     "~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.ByHost.js",
+                                     "~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.Overview.js",
+                                     "~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.Mobile.js",
+                                     "~/Assets/js/status/Dashboards/TLRGRP.BADGER.Dashboard.ByPage.js");
 
-            dashboardsBundle.Include("~/assets/js/status/TLRGRP.BADGER.StopStart.js",
-                                     "~/assets/js/status/TLRGRP.BADGER.MetricRequest.js");
+            dashboardsBundle.Include("~/Assets/js/status/TLRGRP.BADGER.StopStart.js",
+                                     "~/Assets/js/status/TLRGRP.BADGER.MetricRequest.js");
             return dashboardsBundle;
         }
     }
