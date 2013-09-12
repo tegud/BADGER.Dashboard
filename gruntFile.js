0585f6e64d09d99d7a8d6606231f559909d2e7f1
Steve Elliott <steve@tegud.net>
2013-09-12 21:51:20 +0100

    Ignoring minification for now

--- gruntFile.js
+++ gruntFile.js
@@ -1,27 +1,17 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        files: [{
-            expand: true,
-            src: '**/*.js',
-            dest: 'Badger.Dashboard/Assets/js/src',
-            cwd: 'Badger.Dashboard/Assets/js/build'
-        }]
-      }
+    jshint: {
+      all: ['Badger.Dashboard/Assets/js/**/*.js', 'tests/**/*.js']
     },
-  jshint: {
-    all: ['Badger.Dashboard/src/*.js', 'tests/**/*.js']
-  },
-  qunit: {
-    all: ['tests/**/*.html']
-  }
+    qunit: {
+      all: ['tests/**/*.html']
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'qunit']);
 };
