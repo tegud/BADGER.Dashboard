3496170bb7ee84810a3be491d235781e802a7ab8
Steve Elliott <steve@tegud.net>
2013-11-26 00:37:09 +0000

    Start of error component

---
+++ Badger.Dashboard/Assets/js/v2/Components/Errors/TLRGRP.BADGER.Components.ErrorsByHour.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+
+    TLRGRP.namespace('TLRGRP.BADGER.Dashboard.Components');
+
+    TLRGRP.BADGER.Dashboard.Components.ErrorsByHour = function (configuration) {
+        var componentLayout = new TLRGRP.BADGER.Dashboard.ComponentModules.ComponentLayout({
+            title: 'By Hour',
+            componentClass: 'errors-by-hour-selector',
+            modules: [
+                {
+                    appendTo: function (componentElement) {
+                        var hours = [];
+
+                        function padHour(hour) {
+                            if (hour < 10) {
+                                return '0' + hour;
+                            }
+                            return hour;
+                        }
+
+                        for (var x = 0; x < 24; x++) {
+                            var start = padHour(x) + ':00';
+                            var end = x == 23 ? '23:59' : padHour(x + 1) + ':00';
+
+                            hours.push({
+                                start: start,
+                                end: end
+                            });
+                        }
+
+                        componentElement.append(Mustache.render('<ul class="errors-by-hour-list">{{#hours}}<li><a href=""><div class="errors-by-hour-link">{{start}} - {{end}}</div><div class="errors-by-hour-counter">9999</div><div class="errors-by-hour-graph"></div></a></li>{{/hours}}</ul>', {
+                            hours: hours
+                        }));
+                    }
+                }
+            ]
+        });
+        
+        var stateMachine = nano.Machine({
+            states: {
+                uninitialised: {
+                    initialise: function (container) {
+                        componentLayout.appendTo(container);
+                        this.transitionToState('initialised');
+                    }
+                },
+                initialised: {
+                    _onEnter: function () {
+                    }
+                }
+            },
+            initialState: 'uninitialised'
+        });
+
+        return {
+            render: function (container) {
+                return stateMachine.handle('initialise', container);
+            },
+            unload: function () {
+                stateMachine.handle('stop');
+                stateMachine.handle('remove');
+            }
+        };
+    };
+})();
+
