65e70dde30f512142537309e38a71f9b93874317
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2014-03-20 11:08:27 +0000

    Add Live Event viewer page

---
+++ Badger.Dashboard/Controllers/LiveEventController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BADGER.Dashboard.Controllers
+{
+    public class LiveEventController : Controller
+    {
+        //
+        // GET: /LiveEvent/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
